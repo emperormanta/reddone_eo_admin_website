f2e6d338cc8f2c36eeb8861282fd0030a7bdb23d
Willy Tedjakusuma <emperormanta@gmail.com>
2018-04-26 17:19:36 +0700

    Fixed merge conflict and create about_us module

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
         post :import
       end
     end
+    resources :about
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_19_105539) do
+ActiveRecord::Schema.define(version: 2018_04_24_122207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abouts", force: :cascade do |t|
+    t.text "description"
+    t.boolean "active"
+    t.integer "created_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "guests", force: :cascade do |t|
     t.string "guest_id"
